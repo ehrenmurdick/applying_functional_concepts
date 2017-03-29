f24b8248dcbd07889b6ea215aab012905f37c59d
Pivotal <pivotal@lankershim.la.pivotallabs.com>
2017-03-29 16:09:43 -0700

    added simpler higher order example

--- code/higher_order0.js
+++ code/higher_order0.js
@@ -3,6 +3,7 @@
 // => [2, 4, 6]
 // =end
 
-console.assert(2 == [1, 2, 3].map((x) => x * 2)[0])
-console.assert(4 == [1, 2, 3].map((x) => x * 2)[1])
-console.assert(6 == [1, 2, 3].map((x) => x * 2)[2])
+let r = [1, 2, 3].map((x) => x * 2)
+let e = [2, 4, 6]
+// quick and dirty, don't judge!
+console.assert(JSON.stringify(e) === JSON.stringify(r))
